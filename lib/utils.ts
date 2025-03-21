3377cd868bf60fecea247de0d67ef0f2b8191813
Photis Avrilionis <6431229+pavril@users.noreply.github.com>
2025-03-21 10:44:21 +0200

    Created `replace` task

---
+++ lib/utils.ts
@@ -0,0 +1,90 @@
+/*
+ * DCAPv2 Token Smart Contracts
+ * Copyright (C) 2025  Compellio S.A.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import CliTable3 from "cli-table3";
+import {HardhatRuntimeEnvironment} from "hardhat/types";
+
+export function extractTokenProperties(tokenId: string) {
+    const parts = tokenId.match(/urn:tar:eip155.(?<chainId>\d+):(?<address>[0-9a-fA-F]{40})/);
+
+    if (parts === null) {
+        throw new Error(`Invalid token id: ${tokenId}`);
+    }
+
+    return {
+        tokenId: tokenId,
+        chainId: BigInt(parseInt(parts?.groups?.chainId!)),
+        address: "0x".concat(parts?.groups?.address!)
+    }
+}
+
+export async function getNetworkChainId(hre: HardhatRuntimeEnvironment, name: string) {
+
+    if ("chainId" in hre.config.networks[name] && hre.config.networks[name].chainId) {
+        return BigInt(hre.config.networks[name].chainId);
+    }
+
+    if ("url" in hre.config.networks[name] && hre.config.networks[name].url) {
+        const provider = new hre.ethers.JsonRpcProvider(hre.config.networks[name].url);
+        const network = await provider.getNetwork();
+
+        return network.chainId;
+    }
+
+    throw new Error(`Cannot find chain id of ${name}. Please set it manually in hardhat.config.ts.`);
+
+}
+
+export async function getNetworkProvider(hre: HardhatRuntimeEnvironment, chainId: bigint) {
+    const networks = Object.keys(hre.config.networks);
+
+    for (const network of networks) {
+        if (await getNetworkChainId(hre, network) === chainId) {
+            return {
+                name: network,
+                config: hre.config.networks[network],
+                url: "url" in hre.config.networks[network] ? hre.config.networks[network].url : undefined
+            }
+        }
+    }
+
+    throw new Error(`No configured network with chain id ${chainId}`);
+}
+
+export function createCliTable() {
+    return new CliTable3({
+        chars: {
+            'top': '',
+            'top-mid': '',
+            'top-left': '',
+            'top-right': '',
+            'bottom': '',
+            'bottom-mid': '',
+            'bottom-left': '',
+            'bottom-right': '',
+            'left': '',
+            'left-mid': '',
+            'mid': '',
+            'mid-mid': '',
+            'right': '',
+            'right-mid': '',
+            'middle': ''
+        },
+        style: {'padding-left': 0, 'padding-right': 1}
+    });
+}
---
+++ tasks/replace.ts
@@ -0,0 +1,71 @@
+/*
+ * DCAPv2 Token Smart Contracts
+ * Copyright (C) 2025  Compellio S.A.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import {task} from "hardhat/config";
+import {createCliTable, extractTokenProperties, getNetworkChainId, getNetworkProvider} from "../lib/utils";
+
+task("replace", "Attempt to replace a token with is successor")
+    .addPositionalParam("predecessor", "The Token Id of the predecessor token")
+    .addPositionalParam("successor", "The Token Id of the successor token.")
+    .addFlag("force", "Skip validations and forge-update the predecessor.")
+    .setAction(async (params, hre) => {
+
+        const predecessor = extractTokenProperties(params.predecessor);
+        const successor = extractTokenProperties(params.successor);
+
+        if (predecessor.chainId !== await getNetworkChainId(hre, hre.network.name)) {
+            throw new Error("selected network invalid. You must switch to the predecessor's network using --network.");
+        }
+
+        if (!params.force) {
+            // Verify the successor points to its predecessor
+
+            const successorNetwork = await getNetworkProvider(hre, successor.chainId);
+            const DCAPToken = await hre.ethers.getContractFactory("DCAPToken");
+
+            if (!successorNetwork.url) {
+                throw new Error("successor network configured without a URL. Use --force to skip this check.")
+            }
+
+            const provider = new hre.ethers.JsonRpcProvider(successorNetwork.url);
+            const successorContract = DCAPToken.connect(provider).attach(successor.address);
+
+            //@ts-expect-error ts contract definitions buggy when using factories
+            if (predecessor.tokenId !== await successorContract.predecessor()) {
+                throw new Error(`successor token ${successor.tokenId} points to another predecessor (${predecessor.tokenId})`)
+            }
+        }
+
+        const predecessorContract = await hre.ethers.getContractAt("DCAPToken", predecessor.address);
+
+        const response = await predecessorContract.replace(successor.tokenId);
+        const transaction = await response.getTransaction();
+
+        const table = createCliTable();
+
+        table.push(
+            {'Updated Token (predecessor):': `${predecessor.tokenId} (contract ${predecessor.address})`},
+            {"Transaction Hash:": transaction?.hash},
+            {},
+            {'Successor Id:': successor.tokenId},
+        );
+
+        console.log("Predecessor token replaced\n");
+        console.log(table.toString());
+
+    });
