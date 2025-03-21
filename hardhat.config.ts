7fe7ea056a5452cc0c7f093d8c4fa29fe77a2099
Photis Avrilionis <6431229+pavril@users.noreply.github.com>
2025-03-21 10:44:02 +0200

    Configured Polygonscan verification for `amoy`

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,3 +1,21 @@
+/*
+ * DCAPv2 Token Smart Contracts
+ * Copyright (C) 2025  Compellio S.A.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import {HardhatUserConfig} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import {HttpNetworkUserConfig} from "hardhat/types";
@@ -15,7 +33,7 @@ const vargs = yargs
 
 dotenv.config();
 
-const {OWNER_KEY, ETHERSCAN_API_KEY, ETHERLINK_BLOCKSCOUT_API_KEY, TAR_JSON_RPC_URL} = process.env;
+const {OWNER_KEY, ETHERSCAN_API_KEY, ETHERLINK_BLOCKSCOUT_API_KEY, POLYGONSCAN_API_KEY, TAR_JSON_RPC_URL} = process.env;
 
 const sharedNetworkConfig: HttpNetworkUserConfig = {}
 
@@ -33,6 +51,10 @@ const etherscanVerificationApiKeys: Record<string, string> = {
 
 if (ETHERSCAN_API_KEY) {
     etherscanVerificationApiKeys.sepolia = ETHERSCAN_API_KEY;
+}
+
+if (POLYGONSCAN_API_KEY) {
+    etherscanVerificationApiKeys.amoy = POLYGONSCAN_API_KEY;
 }
 
 const config: HardhatUserConfig = {
@@ -65,6 +87,14 @@ const config: HardhatUserConfig = {
                 urls: {
                     apiURL: "https://testnet.explorer.etherlink.com/api",
                     browserURL: "https://testnet.explorer.etherlink.com/"
+                }
+            },
+            {
+                network: 'amoy',
+                chainId: 80002,
+                urls: {
+                    apiURL: "https://api-amoy.polygonscan.com/api",
+                    browserURL: "https://amoy.polygonscan.com/"
                 }
             }
         ]
